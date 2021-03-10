0210d6e783d65552867b88e249ff3c8340e33065
Andre <andre.almeida@instanda.com>
2021-03-10 08:21:12 +0000

    enforce newData from mounted component

--- src/components/PoC/CssJsEditorPoC.tsx
+++ src/components/PoC/CssJsEditorPoC.tsx
@@ -6,6 +6,7 @@ import 'codemirror/lib/codemirror.css';
 import 'codemirror/theme/material.css';
 
 type CssJsEditorState = {
+  key: string,
   html: string,
 };
 
@@ -16,7 +17,7 @@ type CssJsEditorProps = {
 const options = {
   mode: 'xml',
   theme: 'material',
-  lineNumbers: true
+  lineNumbers: true,
 };
 
 class CssJsEditorPoC extends Component<CssJsEditorProps, CssJsEditorState> {
@@ -24,23 +25,37 @@ class CssJsEditorPoC extends Component<CssJsEditorProps, CssJsEditorState> {
     super(props);
 
     this.state = {
+      key: '',
       html: '',
       //templateId: 'fd4ede1b-169d-4366-8e2c-3f233d0c505a',
       //templateId: 'c2a95458-c0af-443e-86db-a5fe3678e192',
     };
   }
 
-  componentDidMount() {
+  fetchData() {
     axios.get(`${process.env.REACT_APP_DESIGN_GATEWAY_URL}/templates/${this.props.templateId}`)
     .then(response => {
       //log off based on response. How can a non error response be invalid? Maybe only after
-      this.setState({html: response.data.html});
+      this.setState({ html: response.data.html, key: this.props.templateId});
     },
     (error) => {
       console.log(error);
       // alert with error message
       // which error message might also need for logOff()?
     });
+  };
+
+  componentDidMount() {
+    console.log("component did mount");
+    this.fetchData();
+  };
+
+  componentDidUpdate() {
+    console.log("component did update");
+    if(this.props.templateId != this.state.key){
+      this.fetchData();
+      console.log("updating");
+    }
   };
   
   handleEditorChange = (editor: any, data: any, value:any) => {
