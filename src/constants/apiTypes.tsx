f8fb3bf4caca8b5f191053a8f92b7789fc20f7d2
Pedro Almeida <pedro.almeida@instanda.com>
2021-11-09 09:08:35 +0000

    Merged PR 12109: CLM-354 Handle of validation for Create package

--- src/constants/apiTypes.tsx
+++ src/constants/apiTypes.tsx
@@ -1,56 +1,63 @@
 type DateTimestamp = {
-  seconds: number,
-  nanos: number,
+  seconds: number;
+  nanos: number;
 };
 
 export type apiPackage = {
-  id: string,
-  name: string,
-  clientId: string,
-  packageId: string,
-  createdDate: DateTimestamp,
+  id: string;
+  name: string;
+  clientId: string;
+  packageId: string;
+  createdDate: DateTimestamp;
 };
 
 export type apiSite = {
-  id: string,
-  siteId: string,
-  name: string,
-  subDomain: string,
-  audience: string,
-  clientId: string,
-  packages: Array<string>,
-  createdDate: DateTimestamp,
+  id: string;
+  siteId: string;
+  name: string;
+  subDomain: string;
+  audience: string;
+  clientId: string;
+  packages: Array<string>;
+  createdDate: DateTimestamp;
 };
 
 export type NumberRange = {
   start: number;
   end: number;
-}
+};
 
 export type LetterRange = {
   start: string;
   end: string;
-}
+};
 
 export type apiNumberFormat = {
-  id: string,
-  numberFormatId: string,
-  packageId: string,
-  clientId: string,
-  livePrefix: string,
-  testPrefix: string,
-  suffix: string,
-  numberPadding: number,
-  numberRange: NumberRange[],
-  letterRange: LetterRange[],
-  messageIfNumbersOutsideRange: string,
-  messageIfLettersOutsideRange: string,
+  id: string;
+  numberFormatId: string;
+  packageId: string;
+  clientId: string;
+  livePrefix: string;
+  testPrefix: string;
+  suffix: string;
+  numberPadding: number;
+  numberRange: NumberRange[];
+  letterRange: LetterRange[];
+  messageIfNumbersOutsideRange: string;
+  messageIfLettersOutsideRange: string;
 };
 
 export type apiClaimStatuses = {
-  id: string,
-  claimStatusesId: string,
-  packageId: string,
-  clientId: string,
-  claimStatusesList: Array<string>,
+  id: string;
+  claimStatusesId: string;
+  packageId: string;
+  clientId: string;
+  claimStatusesList: Array<string>;
+};
+
+export type ValidationErrorEntry = {
+  type: string;
+  code: string;
+  message: string;
+  property: string;
 };
--- src/pages/Packages/CreatePackage.tsx
+++ src/pages/Packages/CreatePackage.tsx
@@ -1,100 +1,106 @@
-import React, { useState, ChangeEvent } from "react";
-import axios from "axios";
-import {
-  Paper,
-  TextField,
-  Typography,
-  makeStyles,
-} from "@material-ui/core";
+import React, { useState, ChangeEvent } from 'react';
+import axios from 'axios';
+import { Paper, TextField, Typography, makeStyles } from '@material-ui/core';
 
-import { RootState } from "../../store";
-import { ContentLayout } from "../../components/Layout";
-import { useSelector } from "react-redux";
-import { useHistory } from "react-router";
-import SaveAndCancelButtons from "../../components/Buttons/SaveAndCancelButtons";
+import { RootState } from '../../store';
+import { ContentLayout } from '../../components/Layout';
+import { useSelector } from 'react-redux';
+import { useHistory } from 'react-router';
+import SaveAndCancelButtons from '../../components/Buttons/SaveAndCancelButtons';
+import { ValidationErrorEntry } from '../../constants/apiTypes';
 
 const useStyles = makeStyles(() => ({
   form: {
-    padding:20,
-    marginBottom:20  
+    padding: 20,
+    marginBottom: 20,
   },
 }));
 
-const CreatePackage = () => {  
+const CreatePackage = () => {
   const [packageName, setPackageName] = useState('');
-  const [nameError, setNameError] = useState('');
+
+  const [validationErrors, setValidationErrors] = useState<{
+    [id: string]: string;
+  }>({});
+
   const accessToken = useSelector((state: RootState) => state.auth.accessToken);
   const history = useHistory();
   const classes = useStyles();
 
   const handleCancelPackageButton = () => {
-    history.push("/packages");
-  }
-  
+    history.push('/packages');
+  };
+
   const handleTextChange = (e: ChangeEvent<HTMLInputElement>) => {
     setPackageName(e.target.value);
-    setNameError('');
-  }
+    setValidationErrors((x) => ({ ...x, [e.target.name]: '' }));
+  };
 
   const handleSavePackageButton = () => {
     const body = {
-      name: packageName
+      name: packageName,
     };
 
-    axios.post(`${process.env.REACT_APP_DESIGN_GATEWAY_URL}/Packages`,
-      body,
-      { headers: {
-         ContentType: 'application/json',
-         Authorization: `Bearer ${accessToken}`
-        }         
+    axios
+      .post(`${process.env.REACT_APP_DESIGN_GATEWAY_URL}/Packages`, body, {
+        headers: {
+          ContentType: 'application/json',
+          Authorization: `Bearer ${accessToken}`,
+        },
       })
-    .then(() => {
-      //created    
-      history.push('/packages');
-    },
-    (error) => {      
-      if (error.response){    
-        setNameError(error.response.data);
-      }else
-      {
-        //Generic error
-        setNameError("Something went wrong, please try again.");
-      }  
-    });
+      .then(
+        () => {
+          //created
+          history.push('/packages');
+        },
+        (error) => {
+          if (error.response) {
+            if (error.response.data.HasValidations) {
+              const data: Array<ValidationErrorEntry> = error.response.data.validationEntries;
+
+              var errorArray: { [id: string]: string } = {};
+
+              data.forEach((x) => {
+                errorArray[x.property] = x.message;
+              });
+
+              setValidationErrors(errorArray);
+            } else if (error.response.data.HasErrors) {
+              //Generic error
+              //TODO: Decide how we present them to the user
+            }
+          } else {
+          }
+        }
+      );
   };
 
   return (
-    <ContentLayout 
-      title="Create Package"
-      data-test="component-create-package" 
-    >
-      <Paper elevation={2} >
+    <ContentLayout title="Create Package" data-test="component-create-package">
+      <Paper elevation={2}>
         <div className={classes.form}>
-          <Typography 
-            data-test="create-package-name-label"
-            variant="subtitle1"
-            align="left"
-          >
+          <Typography data-test="create-package-name-label" variant="subtitle1" align="left">
             Name
           </Typography>
-          <TextField  
+          <TextField
+            name="Name"
             data-test="create-package-name-input"
-            error={nameError !== ""}
-            helperText={nameError}  
-            size="small" 
+            error={validationErrors['Name'] !== undefined && validationErrors['Name'] !== ''}
+            helperText={validationErrors['Name']}
+            size="small"
             required={true}
-            onChange= {handleTextChange} 
+            onChange={handleTextChange}
             variant="outlined"
             fullWidth
-          />    
+          />
         </div>
-        <SaveAndCancelButtons 
+        <SaveAndCancelButtons
           data-test="save-cancel-buttons"
           saveButtonLabel="Save package"
           handleSaveButton={handleSavePackageButton}
           handleCancelButton={handleCancelPackageButton}
         />
-      </Paper>      
+      </Paper>
     </ContentLayout>
   );
 };
