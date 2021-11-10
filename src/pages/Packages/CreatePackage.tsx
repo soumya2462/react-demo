5f38f9a7b9bc2a6bc2210b647f15794ea8e7d7f5
Pedro Almeida <pedro.almeida@instanda.com>
2021-11-10 17:47:56 +0000

    Merged PR 12154: Checking UI deployment

--- src/pages/Packages/CreatePackage.tsx
+++ src/pages/Packages/CreatePackage.tsx
@@ -41,6 +41,8 @@ const CreatePackage = () => {
       name: packageName,
     };
 
+    console.log('POSTING');
+
     axios
       .post(`${process.env.REACT_APP_DESIGN_GATEWAY_URL}/Packages`, body, {
         headers: {
