750c852c7c495c85cce6486c3a4b1943f9b9bf3e
Pedro Almeida <pedro.almeida@instanda.com>
2021-11-12 14:42:06 +0000

    Merged PR 12186: Routing fix for siteId

--- src/pages/Sites/EditSite.tsx
+++ src/pages/Sites/EditSite.tsx
@@ -1,58 +1,58 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
-import { useSelector } from "react-redux";
-import { useHistory } from "react-router";
-import { useParams } from "react-router-dom";
-import { RootState } from "../../store";
-import { apiSite } from "../../constants/apiTypes";
-import SiteSetup from "../../components/SiteConfig/SiteSetup";
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { useSelector } from 'react-redux';
+import { useHistory } from 'react-router';
+import { useParams } from 'react-router-dom';
+import { RootState } from '../../store';
+import { apiSite } from '../../constants/apiTypes';
+import SiteSetup from '../../components/SiteConfig/SiteSetup';
 
 type paramsType = {
-  siteid: string,
-}
+  siteId: string;
+};
 
-const EditSite = () => {  
+const EditSite = () => {
   const [siteToEdit, setSiteToEdit] = useState<apiSite>({
-    name: "",
-    subDomain: "",
-    id: "",
-    siteId: "",
-    audience: "undefined",
-    clientId: "",
+    name: '',
+    subDomain: '',
+    id: '',
+    siteId: '',
+    audience: 'undefined',
+    clientId: '',
     packages: [],
     createdDate: {
       seconds: 0,
       nanos: 0,
     },
-  });  
+  });
 
-  const [validationError, setValidationError] = useState(''); 
+  const [validationError, setValidationError] = useState('');
   const accessToken = useSelector((state: RootState) => state.auth.accessToken);
   const history = useHistory();
-  const { siteid } = useParams<paramsType>();
+  const { siteId } = useParams<paramsType>();
 
-  useEffect(() => {       
-    axios.get(`${process.env.REACT_APP_DESIGN_GATEWAY_URL}/sites/${siteid}`,      
-    { 
-      headers: {
-        ContentType: 'application/json',
-        Authorization: `Bearer ${accessToken}`
-      }        
-    })
-    .then(response => {  
-      setSiteToEdit(response.data);
-    },
-    (error) => {   
-      if (error.response){    
-        setValidationError(error.response.data);
-      }
-      else
-      {
-        //Generic error
-        setValidationError("Something went wrong, please try again.");
-      }
-    });
-  }, [accessToken, siteid]);
+  useEffect(() => {
+    axios
+      .get(`${process.env.REACT_APP_DESIGN_GATEWAY_URL}/sites/${siteId}`, {
+        headers: {
+          ContentType: 'application/json',
+          Authorization: `Bearer ${accessToken}`,
+        },
+      })
+      .then(
+        (response) => {
+          setSiteToEdit(response.data);
+        },
+        (error) => {
+          if (error.response) {
+            setValidationError(error.response.data);
+          } else {
+            //Generic error
+            setValidationError('Something went wrong, please try again.');
+          }
+        }
+      );
+  }, [accessToken, siteId]);
 
   const handleSaveSiteButton = (site: apiSite, packagesValue: string) => {
     const body = {
@@ -60,41 +60,40 @@ const EditSite = () => {
       subDomain: site.subDomain,
       audience: site.audience,
       packages: site.packages,
-      siteId: site.siteId
+      siteId: site.siteId,
     };
-    
-    if(packagesValue === "site-packages-some" && site.packages.length === 0)
-    {
-      setValidationError("Please select at least one package");
+
+    if (packagesValue === 'site-packages-some' && site.packages.length === 0) {
+      setValidationError('Please select at least one package');
       return false;
     }
 
-    axios.put(`${process.env.REACT_APP_DESIGN_GATEWAY_URL}/sites`,
-      body,
-      { headers: {
-         ContentType: 'application/json',
-         Authorization: `Bearer ${accessToken}`
-        }         
+    axios
+      .put(`${process.env.REACT_APP_DESIGN_GATEWAY_URL}/sites`, body, {
+        headers: {
+          ContentType: 'application/json',
+          Authorization: `Bearer ${accessToken}`,
+        },
       })
-    .then(() => {
-      //created    
-      history.push('/sites');
-    },
-    (error) => {     
-      
-      if (error.response){    
-        setValidationError(error.response.data);
-      }else
-      {
-        //Generic error
-        setValidationError("Something went wrong, please try again.");
-      }  
-    });
+      .then(
+        () => {
+          //created
+          history.push('/sites');
+        },
+        (error) => {
+          if (error.response) {
+            setValidationError(error.response.data);
+          } else {
+            //Generic error
+            setValidationError('Something went wrong, please try again.');
+          }
+        }
+      );
   };
 
   return (
-    <SiteSetup 
-      data-test="component-edit-site" 
+    <SiteSetup
+      data-test="component-edit-site"
       saveButtonLabel="Save site"
       handleSaveSiteButton={handleSaveSiteButton}
       validationError={validationError}
